b012254313325df6a3f93b553d3f3ffa9b102f8c
Saqlain Mahmud <sqlnmhmdkhanemon@gmail.com>
2020-02-22 11:31:37 +0600

    .gitignore is now working

--- build/utils.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.dateStringToDate = function (dateString) {
-    var datePart = dateString.split("/").map(function (datePart) {
-        return parseInt(datePart);
-    });
-    return new Date(datePart[2], datePart[1] - 1);
-};
